86ee276aacb79bd870dfab449bdcf0a00a2cbaae
niks1503 <niksharma1503@gmail.com>
2024-10-23 17:44:20 +0545

    dom1 project created

---
+++ script.js
@@ -0,0 +1,10 @@
+let div = document.getElementById("div-container");
+
+div.style.background = "green";
+div.style.color = "white";
+div.style.margin = "20px";
+div.style.padding = "10px";
+div.style.fontSize = "18px";
+div.style.fontWeight = "bold";
+div.style.height = "200px";
+div.style.width = "300px";
